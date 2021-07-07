77bff19b6c809f469850b0896bbf062ff64caa9b
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-07-07 11:08:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -63,3 +63,78 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  wrapper: document.querySelector('.lightbox'),
+  wrapperOverlay: document.querySelector('.lightbox__overlay'),
+  wrapperContent: document.querySelector('.lightbox__content'),
+  wrapperImg: document.querySelector('.lightbox__image'),
+  closeWrapperButton: document.querySelector('.lightbox__button'),
+};
+
+/*Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.*/
+
+console.log(refs.wrapperImg);
+
+const galleryItemsMarkup = createElGalleryMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createElGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class = "gallery__item">
+      <a
+        class = "gallery__link"
+        href = ${original}
+      >
+        <img
+          class = "gallery__image"
+          src = "${preview}"
+          data-source = "${original}"
+          alt = "${description}"
+        />
+      </a>
+    </li>
+    `;
+    })
+    .join('');
+}
+console.log(galleryItems);
+
+/*  Отмена действия по умолчанию при клике на ссылку */
+
+refs.gallery.addEventListener('click', function (evt) {
+  evt.preventDefault();
+});
+
+/* Реализация делегирования на галерее ul.js-gallery и получение url большого изображения */
+
+refs.gallery.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  const isGalleryEl = evt.target.classList.contains('gallery__image');
+  if (!isGalleryEl) {
+    return;
+  }
+
+  /* Открытие модального окна по клику на элементе галереи. */
+  refs.wrapper.classList.add('is-open');
+
+  /* Подмена значения атрибута src, alt элемента img.lightbox__image. */
+  refs.wrapperImg.src = evt.target.dataset.source;
+  refs.wrapperImg.alt = evt.target.alt;
+  console.log(evt.target.dataset.source);
+  console.log(evt.target.alt);
+}
+
+/* Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]. */
+refs.closeWrapperButton.addEventListener('click', onCloseWrapper);
+
+function onCloseWrapper() {
+  refs.wrapper.classList.remove('is-open');
+
+  /*Очистка значения атрибута src элемента img.lightbox__image.*/
+  refs.wrapperImg.src = '';
+}
